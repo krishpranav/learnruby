022767bb9e029c44245849759789a77a75bbf0f0
krishpranav <krisna.pranav@gmail.com>
2021-10-02 19:30:53 +0530

    modules

---
+++ modules/modules.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+module Modules
+
+    def Modules.helloworldone()
+        puts "hello world one"
+    end
+
+    def Modules.helloworldtwo()
+        puts "hello world two"
+    end
+
+end
+
+Modules.helloworldone()
+Modules.helloworldtwo()
