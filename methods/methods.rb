7159bd1df9b82205962552e35a84bb3c0fea4988
krishpranav <krisna.pranav@gmail.com>
2021-10-02 19:27:01 +0530

    methods

---
+++ methods/methods.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+def test(a1 = "Ruby", a2 = "Python")
+    puts "The programming language is #{a1}"
+    puts "The programming language is #{a2}"
+end
+
+# test "C", "C++"
+test "C++", "Golang" 
+test
