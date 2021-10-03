8ddfd065e7c0ba2b3407e109e711ef23796e108a
krishpranav <krisna.pranav@gmail.com>
2021-10-03 13:35:09 +0530

    ruby-advanced: socket

---
+++ ruby-advanced/socket/socket.rb
@@ -0,0 +1,13 @@
+require 'net/http'
+
+host = 'www.google.com'
+path = '/index.html'
+
+http = Net::HTTP.new(host)
+headers, body = http.get(path)
+
+if headers.code == "200"
+    print body
+else
+    puts "#{headers.code} #{headers.message}"
+end
